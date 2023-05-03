118bc1e4e35dec9738ee3d518ec7737664a7bdd4
Patryk Panek <patryyk221@gmail.com>
2023-05-03 15:34:04 +0200

    Change routing

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Hellowdwqdqwddwqdqwdwqdqdqwdqdqdw
+         Hellowdwqdqwddwqdqwdwqdqdqwdqdqdwwdqwdwqdqw
         </a>
       </header>
     </div>
